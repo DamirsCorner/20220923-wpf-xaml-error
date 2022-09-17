bbf4735524a743e2667df59ae683ed8cc1e32a6f
Damir Arh <damir.arh@gmail.com>
2022-09-17 11:19:23 +0200

    Add project files.

---
+++ XamlError/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace XamlError;
+
+public partial class App : Application
+{
+    public App()
+    {
+        DispatcherUnhandledException += App_DispatcherUnhandledException;
+    }
+
+    private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        var exception = e.Exception;
+        e.Handled = true;
+        Debug.WriteLine(exception.ToString());
+        MessageBox.Show(exception.Message);
+        Shutdown();
+    }
+}
